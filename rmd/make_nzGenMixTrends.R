4abcd8880c30eae4389610890549fd3797eaf63b
Ben Anderson <dataknut@icloud.com>
2024-06-26 20:03:38 +1200

    typos

--- rmd/make_nzGenMixTrends.R
+++ rmd/make_nzGenMixTrends.R
@@ -5,8 +5,10 @@ library(gridCarbon)
 # comes in multiple files - load them in the .rmd
 gridCarbon::get_nzGenMix(years = seq(2020,2024,1))
 
+# May 2024 data appears to be a duolicate of April 2024
+
 library(here)
-  rmarkdown::render(input = paste0(here::here("rmd", "nzGenMixTrends"), ".Rmd"),
-                    output_file = paste0(here::here("docs/nzGenMixTrends.html"))
-  )
+rmarkdown::render(input = paste0(here::here("rmd", "nzGenMixTrends"), ".Rmd"),
+                  output_file = paste0(here::here("docs/nzGenMixTrends.html"))
+)
 
