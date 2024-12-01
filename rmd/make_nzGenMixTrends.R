28485130f3016deb22763830e5aa511d52f2b26e
Ben Anderson <dataknut@icloud.com>
2024-12-01 15:06:06 +1300

    re-run NZ for 2010 onwards

--- rmd/make_nzGenMixTrends.R
+++ rmd/make_nzGenMixTrends.R
@@ -3,7 +3,8 @@ library(gridCarbon)
 
 # make sure we have all the data
 # comes in multiple files - load them in the .rmd
-gridCarbon::get_nzGenMix(years = seq(2020,2024,1))
+# data files prior to 2020 cause a code error - needs fixing
+gridCarbon::get_nzGenMix(years = seq(2010,2024,1))
 
 library(here)
 rmarkdown::render(input = paste0(here::here("rmd", "nzGenMixTrends"), ".Rmd"),
