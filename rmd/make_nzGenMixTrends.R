8721ce32422afb38c191c8087044861626c38b63
Ben Anderson <dataknut@icloud.com>
2025-03-15 17:06:34 +1300

    latest run (not sure why so many commits...)

--- rmd/make_nzGenMixTrends.R
+++ rmd/make_nzGenMixTrends.R
@@ -4,7 +4,7 @@ library(gridCarbon)
 # make sure we have all the data
 # comes in multiple files - load them in the .rmd
 # data files prior to 2020 cause a code error - needs fixing
-gridCarbon::get_nzGenMix(years = seq(2010,2024,1))
+gridCarbon::get_nzGenMix(years = seq(2010,2025,1))
 
 library(here)
 rmarkdown::render(input = paste0(here::here("rmd", "nzGenMixTrends"), ".Rmd"),
